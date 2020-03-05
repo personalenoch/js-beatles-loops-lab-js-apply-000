8dd898c6ca130118b1db3f17a7c0c3a1183e6369
enoch ajagbe <enoch.ajagbe1@gmail.com>
2020-03-05 22:14:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ var iLoveTheBeatles = function(num) {
     message.push("I love the Beatles!");
     i++;
   } 
-  while (i < num && num < 15); 
+  while (i < num && num < 16); 
   return message;
 }
